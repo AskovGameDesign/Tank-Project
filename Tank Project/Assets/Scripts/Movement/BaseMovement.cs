5815d77a4d52c468b8c8b41181c2981098d2241c
Claus Bjerre Jacobsen <claus@aneks.dk>
2019-04-12 16:20:10 +0200

    temp

--- Tank Project/Assets/Scripts/Movement/BaseMovement.cs
+++ Tank Project/Assets/Scripts/Movement/BaseMovement.cs
@@ -6,11 +6,12 @@ public class BaseMovement : MonoBehaviour
 {
 	public float speed = 5;
 	public float rotationSpeed = 50;
-	public Rigidbody turretRB;
 	public GameObject turret;
 	string horizontalMovementAxis, verticalMovementAxis, horizontalTurretAxis;
 	float horizontalMovement, verticalMovement, horizontalTurret;
 	Rigidbody rb;
+
+    public float turretYRotation = 0f;
 
 	Tank tankStats;
 
@@ -27,15 +28,23 @@ public class BaseMovement : MonoBehaviour
 		{
 			return;
 		}
-		horizontalMovement = Input.GetAxis( tankStats.playerId.ToString() + "_HM");
-		verticalMovement = -Input.GetAxis( tankStats.playerId.ToString() + "_VM");
-		horizontalTurret = Input.GetAxis( tankStats.playerId.ToString() + "_TR");
+        horizontalMovement = Input.GetAxis(tankStats.playerId.ToString() + "_HM"); //Input.GetAxis("Horizontal");// CheapJoyStickHack(Input.GetAxis(tankStats.playerId.ToString() + "_HM"), 0.1f);
+        verticalMovement = -Input.GetAxis(tankStats.playerId.ToString() + "_VM"); //Input.GetAxis("Vertical");//CheapJoyStickHack(-Input.GetAxis(tankStats.playerId.ToString() + "_VM"), 0.1f);
+        horizontalTurret = Input.GetAxis(tankStats.playerId.ToString() + "_TR"); //CheapJoyStickHack(Input.GetAxis(tankStats.playerId.ToString() + "_TR"), 0.4f);
 
-		if (turret)
+        if (turret )
 		{
-			float turretRotation = horizontalTurret * rotationSpeed * Time.deltaTime;
-			Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
-			turret.transform.localRotation *= tr;
+            float turretRotation = horizontalTurret * 80f * Time.deltaTime;
+            Debug.Log(gameObject.name + " " + Mathf.Sign(horizontalTurret));
+
+
+            turretYRotation += horizontalTurret * 80f * Time.deltaTime;
+            Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
+            //turret.transform.localRotation *= Quaternion.identity;
+
+            turret.transform.localRotation *= tr;
+
+         
 
 		}
 
@@ -45,7 +54,6 @@ public class BaseMovement : MonoBehaviour
 	void FixedUpdate()
     {
 		
-
 		Vector3 tankMovement = transform.forward * verticalMovement * speed * Time.deltaTime;
 		rb.MovePosition(rb.position + tankMovement);
 
@@ -54,30 +62,37 @@ public class BaseMovement : MonoBehaviour
 		Quaternion deltarotation = Quaternion.Euler(0f, tankRotation, 0f);
 		rb.MoveRotation(rb.rotation * deltarotation);
 
-/*
+        /*
+       if (turret && turret.GetComponent<Rigidbody>() != null)
+       {
+           float turretRotation = horizontalTurret * rotationSpeed * Time.deltaTime;
+           Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
+           turret.GetComponent<Rigidbody>().MoveRotation(turret.GetComponent<Rigidbody>().rotation * tr);
+       }
 
-		if (turretRB)
-		{
-			float turretRotation = horizontalTurret * rotationSpeed * Time.deltaTime;
-			Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
-			turretRB.MoveRotation(turretRB.rotation * tr);
-		}
-		
-		if (Input.GetKey(KeyCode.W))
-		{
-			rb.AddForce(transform.forward * speed);
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			rb.AddForce(transform.forward * -speed);
-		}
-		if (Input.GetKey(KeyCode.A))
-		{
-			rb.MoveRotation(rb.rotation * deltarotation);
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			rb.MoveRotation(rb.rotation * minusDeltarotaion);
-		}*/
-	}
+       if (Input.GetKey(KeyCode.W))
+       {
+           rb.AddForce(transform.forward * speed);
+       }
+       if (Input.GetKey(KeyCode.S))
+       {
+           rb.AddForce(transform.forward * -speed);
+       }
+       if (Input.GetKey(KeyCode.A))
+       {
+           rb.MoveRotation(rb.rotation * deltarotation);
+       }
+       if (Input.GetKey(KeyCode.D))
+       {
+           rb.MoveRotation(rb.rotation * minusDeltarotaion);
+       }*/
+    }
+
+    float CheapJoyStickHack(float _inputAxis, float _errorCorrection)
+    {
+        if (_inputAxis > _errorCorrection || _inputAxis < -_errorCorrection)
+            return _inputAxis;
+        else
+            return 0f;
+    }
 }
