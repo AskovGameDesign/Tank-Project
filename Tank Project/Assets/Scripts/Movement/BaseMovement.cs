4c275885f47107aa8b0eeaffd0274ee162ea3314
Claus Bjerre Jacobsen <claus@aneks.dk>
2019-04-15 08:44:23 +0200

    Nu med ny tank
    tanken er nu fully operational :-)

--- Tank Project/Assets/Scripts/Movement/BaseMovement.cs
+++ Tank Project/Assets/Scripts/Movement/BaseMovement.cs
@@ -6,7 +6,6 @@ public class BaseMovement : MonoBehaviour
 {
 	public float speed = 5;
 	public float rotationSpeed = 50;
-	public Rigidbody turretRB;
 	public GameObject turret;
 	string horizontalMovementAxis, verticalMovementAxis, horizontalTurretAxis;
 	float horizontalMovement, verticalMovement, horizontalTurret;
@@ -27,7 +26,8 @@ public class BaseMovement : MonoBehaviour
 		{
 			return;
 		}
-		horizontalMovement = Input.GetAxis( tankStats.playerId.ToString() + "_HM");
+		
+        horizontalMovement = Input.GetAxis( tankStats.playerId.ToString() + "_HM");
 		verticalMovement = -Input.GetAxis( tankStats.playerId.ToString() + "_VM");
 		horizontalTurret = Input.GetAxis( tankStats.playerId.ToString() + "_TR");
 
@@ -35,49 +35,19 @@ public class BaseMovement : MonoBehaviour
 		{
 			float turretRotation = horizontalTurret * rotationSpeed * Time.deltaTime;
 			Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
+
 			turret.transform.localRotation *= tr;
-
 		}
-
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
     {
-		
-
 		Vector3 tankMovement = transform.forward * verticalMovement * speed * Time.deltaTime;
 		rb.MovePosition(rb.position + tankMovement);
-
 
 		float tankRotation = horizontalMovement * rotationSpeed * Time.deltaTime;
 		Quaternion deltarotation = Quaternion.Euler(0f, tankRotation, 0f);
 		rb.MoveRotation(rb.rotation * deltarotation);
-
-/*
-
-		if (turretRB)
-		{
-			float turretRotation = horizontalTurret * rotationSpeed * Time.deltaTime;
-			Quaternion tr = Quaternion.Euler(0f, turretRotation, 0f);
-			turretRB.MoveRotation(turretRB.rotation * tr);
-		}
-		
-		if (Input.GetKey(KeyCode.W))
-		{
-			rb.AddForce(transform.forward * speed);
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			rb.AddForce(transform.forward * -speed);
-		}
-		if (Input.GetKey(KeyCode.A))
-		{
-			rb.MoveRotation(rb.rotation * deltarotation);
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			rb.MoveRotation(rb.rotation * minusDeltarotaion);
-		}*/
 	}
 }
